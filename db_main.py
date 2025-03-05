a6db0b928504b7a1b57924a47e7a1b036c0a546b
Gabi-Gindoff <144366612+Gabi-Gindoff@users.noreply.github.com>
2025-03-04 21:05:00 -0500

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -7,33 +7,7 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["turtlebot_db"]
 collection = db["logs"]
 
-def create_document():
-    log_entry = {
-        "timestamp": datetime.utcnow().isoformat() + "Z",
-        "robot_id": "TB3_001",
-        "movement": {
-            "command": "move_forward",
-            "distance_m": 1.5,
-            "speed_mps": 0.2,
-            "position": {"x": 1.0, "y": 1.2, "theta": -0.1}
-        },
-        "sensor": {
-            "LiDAR": {"obstacle_detected": True, "distance_m": 0.5},
-            "IMU": {"acceleration_z": 9.81, "gyro_z": 0.05}
-        },
-        "error": {
-            "code": 101,
-            "message": "Wheel encoder failure"
-        },
-        "action": {
-            "name": "pick_up_object",
-            "status": "success"
-        },
-        "system": {
-            "battery": 85
-        }
-    }
-
+def create_document(data):
     result = collection.insert_one(log_entry)
     print(f'Document inserted with id: {result.inserted_id}')
 
@@ -56,5 +30,32 @@ def delete_document(doc_id):
 
 # Example usage
 if __name__ == "__main__":
-    create_document()  # Insert a new TurtleBot log entry
+
+        log_entry = {
+        "timestamp": "2025-03-04T14:25:00Z",
+        "robot_id": "TB3_001",
+        "movement": {
+            "command": "move_forward",
+            "distance_m": 1.5,
+            "speed_mps": 0.2,
+            "position": {"x": 1.0, "y": 1.2, "theta": -0.1}
+        },
+        "sensor": {
+            "LiDAR": {"obstacle_detected": true, "distance_m": 0.5},
+            "IMU": {"acceleration_z": 9.81, "gyro_z": 0.05}
+        },
+        "error": {
+            "code": 101,
+            "message": "Wheel encoder failure"
+        },
+        "action": {
+            "name": "pick_up_object",
+            "status": "success"
+        },
+        "system": {
+            "battery": 85
+        }
+    }
+    
+    create_document(log_entry)  # Insert a new TurtleBot log entry
     read_documents()
